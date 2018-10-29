1c241e06e3a46858a8d3c21e5382599ee4f93d62
RuofanLiang <nexuslrf@sjtu.edu.cn>
2018-10-29 15:25:48 +0800

    zigbee init

---
+++ include/zigbee_OQPSK/access_code_prefixer.h
@@ -0,0 +1,56 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2018 RuofanLiang@SJTU.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+
+#ifndef INCLUDED_ZIGBEE_OQPSK_ACCESS_CODE_PREFIXER_H
+#define INCLUDED_ZIGBEE_OQPSK_ACCESS_CODE_PREFIXER_H
+
+#include <zigbee_OQPSK/api.h>
+#include <gnuradio/tagged_stream_block.h>
+
+namespace gr {
+  namespace zigbee_OQPSK {
+
+    /*!
+     * \brief <+description of block+>
+     * \ingroup zigbee_OQPSK
+     *
+     */
+    class ZIGBEE_OQPSK_API access_code_prefixer : virtual public gr::tagged_stream_block
+    {
+     public:
+      typedef boost::shared_ptr<access_code_prefixer> sptr;
+
+      /*!
+       * \brief Return a shared_ptr to a new instance of zigbee_OQPSK::access_code_prefixer.
+       *
+       * To avoid accidental use of raw pointers, zigbee_OQPSK::access_code_prefixer's
+       * constructor is in a private implementation
+       * class. zigbee_OQPSK::access_code_prefixer::make is the public interface for
+       * creating new instances.
+       */
+      static sptr make(pad,preamble);
+    };
+
+  } // namespace zigbee_OQPSK
+} // namespace gr
+
+#endif /* INCLUDED_ZIGBEE_OQPSK_ACCESS_CODE_PREFIXER_H */
+
---
+++ swig/zigbee_OQPSK_swig.i
@@ -0,0 +1,16 @@
+/* -*- c++ -*- */
+
+#define ZIGBEE_OQPSK_API
+
+%include "gnuradio.i"			// the common stuff
+
+//load generated python docstrings
+%include "zigbee_OQPSK_swig_doc.i"
+
+%{
+#include "zigbee_OQPSK/access_code_prefixer.h"
+%}
+
+
+%include "zigbee_OQPSK/access_code_prefixer.h"
+GR_SWIG_BLOCK_MAGIC2(zigbee_OQPSK, access_code_prefixer);
